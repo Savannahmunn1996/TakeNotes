60103dd70cf2b63e4a9b67419742a4a373b0d390
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-01-25 10:52:38 -0800

    Get routes for files done

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const app = express();
+const fs = require("fs");
 const path = require("path");
+const api = require("./routes");
 const PORT = process.env.PORT || 5501;
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api", api);
 
 app.use(express.static("public"));
 
 //It looks like I have to make a get request, a post request and a delete request
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
